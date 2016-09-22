f6baec8e996eac0c718333332b8b4251257baaee
caitlin ashtari and CristoVU  Gee <caitlin@>
2016-09-21 20:51:48 -0700

    add conditional statements to js.

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,28 @@ $(document).ready(function(){
    var house = $("input:radio[name=house]:checked").val();
    var character;
 
-   if (animals === "owl"){
-     if (house === "hufflepuff") {
+   if (animals === "owl" && house === "hufflepuff"){
        character = "Hermione";
-     } else if (animals === "owl") {
-       if (house === "gryffindor") {
-         character = "Ron";
-       } else if (animals === "owl") {
-         if (house === slytherin) {
+     } else if (animals === "owl" && house === "gryffindor") {
+         character = "Hermione";
+       } else if (animals === "owl" && house === "slytherin") {
+           character = "Hermione";
+         }
+   if (animals === "snake" && house === "hufflepuff"){
+       character = "Malfoy";
+     } else if (animals === "snake" && house === "gryffindor") {
+         character = "Malfoy";
+       } else if (animals === "snake" && house === "slytherin") {
            character = "Malfoy";
          }
-       }
-     }
-   }
+   if (animals === "rat" && house === "hufflepuff"){
+       character = "Ron";
+     } else if (animals === "rat" && house === "gryffindor") {
+         character = "Ron";
+       } else if (animals === "rat" && house === "slytherin") {
+           character = "Ron";
+         }
 
-
- $("#output").text(character);
+ $("#output").text('Your Harry Potter character is ' + character + '.');
  });
 });
